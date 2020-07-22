f78f983104cfc22017eb3614b7ce860ae38f9e8a
jarjarbring <sevein404@gmail.com>
2020-07-22 15:09:14 +0200

    Added a method to generate ingredients in dropdown menu. Changes table to row/col in recipes

--- src/Modules/recipes/DisplayRecipes.js
+++ src/Modules/recipes/DisplayRecipes.js
@@ -5,6 +5,8 @@ import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 export default class DisplayRecipe extends React.Component {
 
@@ -22,49 +24,85 @@ export default class DisplayRecipe extends React.Component {
         })
     }
 
+    getIngredientList = (ingredientsArray) => {
+        let ingredients = [];
+        for (let i = 0; i < ingredientsArray.length; i++) {
+            ingredients.push(
+                <div>
+                    <li>{ingredientsArray[i].name}: {ingredientsArray[i].quantity}</li>
+                </div>
+            )
+        }
+        return ingredients;
+    }
+
     render() {
 
         const renderArray = [];
+        let changeColor = "";
 
         for (let i = 0; i < this.state.recipeArray.length; i++) {
+            changeColor = (i % 2 === 0 ? "white" : "lightgrey")
             renderArray.push(
-                <tr key={this.state.recipeArray[i].name}>
-                    <td>{i + 1}</td>
-                    <td>
-                        <Dropdown>
-                            <Dropdown.Toggle variant="white" id="dropdown-basic">
-                                {this.state.recipeArray[i].name}
-                            </Dropdown.Toggle>
+
+                <Row className="text-black" >
+                    <Dropdown style={{ width: "100%"}}>
+                        <Dropdown.Toggle className="rounded" id="dropdown-basic" style={{backgroundColor: changeColor, width: "100%" }}>
+       
+                            <Row style={{color: "black"}}>
+                                <Col >{i + 1}</Col>
+                                <Col >{this.state.recipeArray[i].name}</Col>
+                                <Col>{this.state.recipeArray[i].ingredients.length}</Col>
+                            </Row>
+
 
                             <Dropdown.Menu>
-                                <Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">
                                     Instructions
                                     <p>{this.state.recipeArray[i].instructions}</p>
                                 </Dropdown.Item>
 
-                                <Dropdown.Item>
+                                <Dropdown.Item href="/inventory">
                                     Ingredients
                                     <ul>
-                                        <li>
-                                            {this.state.recipeArray[i].ingredients[i].name}: {this.state.recipeArray[i].ingredients[i].quantity}
-                                        </li>
+                                        {this.getIngredientList(this.state.recipeArray[i].ingredients)}
                                     </ul>
                                 </Dropdown.Item>
                                 <Dropdown.Item href="#/action-2">Make</Dropdown.Item>
                             </Dropdown.Menu>
-                        </Dropdown>
 
 
+                        </Dropdown.Toggle>
+                    </Dropdown>
+                </Row>
 
-                    </td>
-                </tr>
 
             )
         }
 
         return (
             <div>
-                <Table striped bordered hover size="sm">
+
+                <Row>
+                    <Col colSpan="6">
+                        <h2 className="text-center">Recipes</h2>
+                    </Col>
+                </Row>
+                <Row colSpan="2" style={{ backgroundColor: "lightblue" }}>
+                    <Col>
+                        #
+                    </Col>
+                    <Col>
+                        Recipe Name
+                    </Col>
+                    <Col>
+                        Number of ingredients
+                    </Col>
+                </Row>
+
+                {renderArray.length ? renderArray : "Loading..."}
+
+                {/* <Table striped bordered hover size="sm">
                     <thead>
                         <tr>
                             <th colSpan="2" style={{ backgroundColor: "lightblue" }}><h2>Recipes</h2></th>
@@ -78,7 +116,7 @@ export default class DisplayRecipe extends React.Component {
                     <tbody>
                         {renderArray.length ? renderArray : "Loading..."}
                     </tbody>
-                </Table>
+                </Table> */}
                 <Button >Add Recipe</Button>
             </div>
         )
